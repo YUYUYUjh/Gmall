eb25475b8453cacf82a8d2d67ceeee4cfc3b24f0
yujinghao <yjhnbb@163.com>
2021-09-28 22:08:01 +0800

    第二天 截止修改

--- service/service_product/src/main/java/com/yy/gmall/product/service/impl/ManageServiceImpl.java
+++ service/service_product/src/main/java/com/yy/gmall/product/service/impl/ManageServiceImpl.java
@@ -7,6 +7,7 @@ import com.yy.gmall.product.mapper.*;
 import com.yy.gmall.product.service.ManageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Wrapper;
 import java.util.List;
@@ -32,12 +33,21 @@ public class ManageServiceImpl implements ManageService {
 
     @Autowired
     private BaseAttrValueMapper baseAttrValueMapper;
+    /**
+     * 获取一级分类
+     * @return
+     */
     @Override
     public List<BaseCategory1> getCategory1() {
         List<BaseCategory1> category1List = baseCategory1Mapper.selectList(null);
         return category1List;
     }
 
+    /**
+     * 获取二级分类
+     * @param category1Id
+     * @return
+     */
     @Override
     public List<BaseCategory2> getCategory2(Long category1Id) {
         QueryWrapper wrapper = new QueryWrapper();
@@ -45,7 +55,11 @@ public class ManageServiceImpl implements ManageService {
         List<BaseCategory2> category2List = baseCategory2Mapper.selectList(wrapper);
         return category2List;
     }
-
+    /**
+     * 获取三级分类
+     * @param category2Id
+     * @return
+     */
     @Override
     public List<BaseCategory3> getCategory3(Long category2Id) {
         QueryWrapper wrapper = new QueryWrapper();
@@ -53,7 +67,13 @@ public class ManageServiceImpl implements ManageService {
         List<BaseCategory3> category3List = baseCategory3Mapper.selectList(wrapper);
         return category3List;
     }
-
+    /**
+     * 根据id获取平台属性
+     * @param category1Id
+     * @param category2Id
+     * @param category3Id
+     * @return
+     */
     @Override
     public List<BaseAttrInfo> getAttrInfoList(Long category1Id, Long category2Id, Long category3Id) {
 
@@ -61,7 +81,11 @@ public class ManageServiceImpl implements ManageService {
 
         return attrInfoList;
     }
-
+    /**
+     * 添加属性值或者修改
+     * @param baseAttrInfo
+     */
+    @Transactional
     @Override
     public void saveAttrInfo(BaseAttrInfo baseAttrInfo) {
         //判断baseAttrInfo是否有id
@@ -89,7 +113,11 @@ public class ManageServiceImpl implements ManageService {
             }
         }
     }
-
+    /**
+     * 根据属性id查询属性
+     * @param attrId
+     * @return
+     */
     @Override
     public List<BaseAttrValue> getAttrValueList(Long attrId) {
         QueryWrapper wrapper = new QueryWrapper();
